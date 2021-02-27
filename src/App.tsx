70142db3509608866122f50926e4b0c7a08b2b99
jarooda <jaluwibowoaji@gmail.com>
2021-02-28 00:34:07 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-gray-200">
+      <p className="text-red-800">HELLO WORLD</p>
+    </div>
+  );
+}
+
+export default App;
